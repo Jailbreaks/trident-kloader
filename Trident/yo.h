1e7604dae059f6688ea12cfd71965e201677cfd5
Mathieu Hautebas <matteyeux@users.noreply.github.com>
2017-03-18 17:11:24 +0100

    Jonathan Seals's fork he shared on Reddit

---
+++ Trident/yo.h
@@ -0,0 +1,13 @@
+//
+//  COCK.h
+//  Trident
+//
+//  Created by notjon on 12/11/16.
+//  Copyright © 2016 Benjamin Randazzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface yo : UIViewController
+
+@end
